1031c795ff84768fb430a7b9056ef96bb259c12e
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-11-04 23:59:03 +0300

    0.9

--- src/pages/ExchangeRates/ExchangeRates.js
+++ src/pages/ExchangeRates/ExchangeRates.js
@@ -8,9 +8,11 @@ import classes from './ExchangeRates.module.scss'
 
 export default function ExchangeRates() {
 
+   console.log(navigator.language)
+
    const RUB_URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
    const EUR_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=f2f2c855dce5a8fc254f3bf2628abdcf'
-   const DATA_URL = (navigator.language === 'ru-RU' || 'ru' ? RUB_URL : EUR_URL)
+   const DATA_URL = (navigator.language === ('ru-RU' || 'ru') ? RUB_URL : EUR_URL)
 
    const [rubData, setRubData] = useState([])
    const [eurData, setEurData] = useState([])
@@ -33,7 +35,7 @@ export default function ExchangeRates() {
       }
    }
 
-   if (navigator.language === 'ru-RU' || 'ru') {
+   if (navigator.language === ('ru-RU' || 'ru')) {
       getValutesRub()
    } else {
       getValutesUsd()
@@ -41,28 +43,35 @@ export default function ExchangeRates() {
 
    return (
       <div className={classes.container}>
-         <h1>{navigator.language === 'ru-RU' || 'ru' ? 'Курс рубля' : 'Euro rate'}</h1>
-         <ul>
-            <ul>
-               {rubData.map(item =>
-                  <li key={item[0]}>
-                     Валюта: {item[0]}, рублей за валюту:{item[1]}
-                  </li>
-               )}
-            </ul>
-            <ul>
-               {eurData.map(item =>
-                  <li key={item[0]}>
-                     Valute: {item[0]}, Rate:{item[1]}
-                  </li>
-               )}
-            </ul>
-         </ul>
-         {navigator.language === 'ru-RU' || 'ru'
-            ? <Button onClick={() => setRubData(rubValues)} text='Получить данные' />
-            : <Button onClick={() => setEurData(eurValues)} text='Get data' />
+         <h1 className={classes.title}>{navigator.language === ('ru-RU' || 'ru') ? 'Курс рубля' : 'Euro rate'}</h1>
+         {
+            navigator.language === ('ru-RU' || 'ru')
+               ?
+               <ul className={classes.rates__list}>
+                  {rubData.map(item =>
+                     <li key={item[0]} className={classes.rates__list__item}>
+                        Валюта: <span>{item[0]}</span>, рублей за валюту:<span>{item[1]}</span>
+                     </li>
+                  )}
+               </ul>
+               :
+               <ul className={classes.rates__list}>
+                  {eurData.map(item =>
+                     <li key={item[0]} className={classes.rates__list__item}>
+                        Valute: <span>{item[0]}</span>, Rate:<span>{item[1]}</span>
+                     </li>
+                  )}
+               </ul>
          }
-         <NavLink to='/'>{navigator.language === 'ru-RU' || 'ru' ? 'Конвертер' : 'Converter'}</NavLink>
+         <div className={classes.rates__button}>
+            {navigator.language === ('ru-RU' || 'ru')
+               ? <Button onClick={() => setRubData(rubValues)} text='Получить данные' />
+               : <Button onClick={() => setEurData(eurValues)} text='Get data' />
+            }
+         </div>
+         <div className={classes.rates__link}>
+            <NavLink to='/'>{navigator.language === ('ru-RU' || 'ru') ? 'Конвертер' : 'Converter'}</NavLink>
+         </div>
       </div>
    )
 }
