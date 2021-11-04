6a268530f2b96e3b24092ff8dd2be5a97f78fd4b
Vladislav Zaporozhets <vladislav.zaporozhets@haiku.dev>
2021-11-04 19:56:17 +0300

    0.6a

--- src/pages/ExchangeRates/ExchangeRates.js
+++ src/pages/ExchangeRates/ExchangeRates.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import axios from "axios"
 import Button from "../../components/Button/Button"
 
@@ -9,8 +9,7 @@ export default function ExchangeRates() {
    const RUB_URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
    const DATA_URL = (navigator.language == 'ru-RU' ? RUB_URL : '')
 
-   // const [valueUsd, setValueUsd] = useState('')
-   // const [valueEur, setValueEur] = useState('')
+   const [appData, setAppData] = useState([])
 
    const arrValues = []
 
@@ -20,42 +19,22 @@ export default function ExchangeRates() {
       for (let i = 0; i < Object.entries(data.data.Valute).length; i++) {
          arrValues.push([(Object.entries(data.data.Valute)[i][1].Name), Object.entries(data.data.Valute)[i][1].Value])
       }
-      // <ul>
-      //    {arrValues.map(item => {
-      //       <li>
-      //          Валюта:{item[0]}:Курс{item[1]}
-      //          {console.log(item[0])}
-      //       </li>
-      //    })}
-      // </ul>
-      // console.log(arrValues[0]);
-      // console.log(Object.entries(data.data.Valute)[2][0]);
-
-      // console.log((Object.entries(data.data.Valute)[0][1].Value));
-      // arrValues.push
-      // setValueUsd(Object.entries(data.data.Valute)[10][1].Value)
-      // setValueEur(Object.entries(data.data.Valute)[11][1].Value)
    }
-
-   // useEffect(() => {
-   //    getValutes()
-   //    console.log(arrValues[0]);
-   // }, [arrValues])
-
-
+   getValutes()
 
    return (
       <div>
          <h1></h1>
          <ul>
-            {arrValues.map(item => {
-               <li>
-                  Валюта:{item[0]}:Курс{item[1]}
-
-               </li>
-            })}
+            <ul>
+               {appData.map(item =>
+                  <li key={item[0]}>
+                     Валюта: {item[0]}:Курс{item[1]}
+                  </li>
+               )}
+            </ul>
          </ul>
-         <Button onClick={getValutes} />
+         <Button onClick={() => setAppData(arrValues)} text='Получить данные' />
       </div>
    )
 }
